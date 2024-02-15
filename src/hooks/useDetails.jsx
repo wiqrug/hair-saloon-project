e0f8738af562cd54cefdb1530b0d68aca977f782
wiqrug <papavasiliou.alexios@gmail.com>
2024-02-15 21:17:17 +0200

    fixed unnecessary useEffect

--- src/hooks/useDetails.jsx
+++ src/hooks/useDetails.jsx
@@ -4,16 +4,10 @@ import { useLanguage } from "../contexts/LanguageContext";
 const useDetails = (translatedText) => {
   const { selectedLanguage, setSelectedLanguage, countries } = useLanguage();
 
-  const [details, setDetails] = useState(
-    translatedText.find((lang) => lang.code == selectedLanguage.code)
+  const details = translatedText.find(
+    (lang) => lang.code == selectedLanguage.code
   );
 
-  useEffect(() => {
-    const newDetails = translatedText.find(
-      (lang) => lang.code == selectedLanguage.code
-    );
-    setDetails(newDetails);
-  }, [selectedLanguage]);
   return { details, selectedLanguage, setSelectedLanguage, countries };
 };
 
