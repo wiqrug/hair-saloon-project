cc604dca9de71684c04abcbd563935c11f506980
wiqrug <papavasiliou.alexios@gmail.com>
2024-02-17 22:22:25 +0200

    setting up navigation with routes..

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import "primeicons/primeicons.css";
 import NavbarTop from "./components/navbar_components/NavbarTop";
 import Navbar from "./components/navbar_components/Navbar";
 import { LanguageProvider } from "./contexts/LanguageContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Photos from "./components/Photos";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
@@ -14,6 +20,15 @@ function App() {
       <LanguageProvider>
         <NavbarTop />
         <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/photos" element={<Photos />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+        </BrowserRouter>
       </LanguageProvider>
     </PrimeReactProvider>
   );
