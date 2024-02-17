438ba295aa3a559aa41b1ebb03a4341d528fce13
wiqrug <papavasiliou.alexios@gmail.com>
2024-02-17 22:29:50 +0200

    fixed some impots

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
-import NavbarTop from "./components/navbar_components/NavbarTop";
-import Navbar from "./components/navbar_components/Navbar";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Services from "./components/Services";
-import Photos from "./components/Photos";
-import AboutUs from "./components/AboutUs";
-import ContactUs from "./components/ContactUs";
+import {
+  Home,
+  Services,
+  Photos,
+  AboutUs,
+  ContactUs,
+  NavbarTop,
+  Navbar,
+} from "./components";
 
 function App() {
   return (
@@ -22,7 +24,8 @@ function App() {
         <Navbar />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Home />} />
+            {/* change this to "/" later on  */}
+            <Route path="/home" element={<Home />} />
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/contact-us" element={<ContactUs />} />
             <Route path="/photos" element={<Photos />} />
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// components/index.js
+export { default as NavbarTop } from "./navbar_components/NavbarTop";
+export { default as Navbar } from "./navbar_components/Navbar";
+export { default as Home } from "./Home";
+export { default as Services } from "./Services";
+export { default as Photos } from "./Photos";
+export { default as AboutUs } from "./AboutUs";
+export { default as ContactUs } from "./ContactUs";
